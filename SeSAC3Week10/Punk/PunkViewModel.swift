12b27e9813ad1b8f601aa5904a334795f997cdad
JongHoon <qnm541@gmail.com>
2023-09-21 09:44:54 +0900

    practice punk view model

--- SeSAC3Week10/Punk/PunkViewModel.swift
+++ SeSAC3Week10/Punk/PunkViewModel.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+
+final class PunkViewModel {
+    
+    func fetchBeers(completion: @escaping ([Beer]) -> Void) {
+        Task {
+            do {
+                let beers = try await PunkManager.shared.punkRequest(of: [Beer].self, api: .getBeers)
+                completion(beers)
+            } catch {
+                print(error)
+            }
+        }
+    }
+    
+    func fetchBeer(id: Int, completion: @escaping (Beer) -> Void) {
+        Task {
+            do {
+                let beer = try await PunkManager.shared.punkRequest(of: Beer.self, api: .getSingleBeer(id: id))
+                completion(beer)
+            } catch {
+                print(error)
+            }
+        }
+    }
+    
+    func randomBeer(completion: @escaping (Beer) -> Void) {
+        Task {
+            do {
+                let beer = try await PunkManager.shared.punkRequest(of: Beer.self, api: .getRandomBeer)
+                completion(beer)
+            } catch {
+                print(error)
+            }
+        }
+    }
+}
